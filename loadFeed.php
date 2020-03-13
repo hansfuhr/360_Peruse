389670c696e56b39a303c39872beecdc4107eb71
Andrew Johnston <adjohnston99@gmail.com>
2020-03-13 15:42:22 -0700

    moved header to header.php other stuff

--- loadFeed.php
+++ loadFeed.php
@@ -12,7 +12,8 @@
 //	// OrderCondition changes depending on hot / new / top etc.
 //	// WHERE clause changes depending on user / all / community etc. feed requested
 //	// $offset should be incremented by LIMIT amount so as to continuously load for data when the end of the feed is reached
-//	$sql = "SELECT *, (/*equation here*/) AS OrderCondition FROM post WHERE /*stuff here*/ ORDER BY OrderCondition DESC LIMIT 10 OFFSET $offset";
+	$sql = "SELECT *, (/*equation here*/) AS OrderCondition FROM post WHERE /*stuff here*/ ORDER BY OrderCondition DESC LIMIT 10 OFFSET $offset";
+	mysqli_query($sql);
 
 
 	echo "<a href=\"post.php?author" + $author + "&timePosted=" + $timePosted + "\">";
