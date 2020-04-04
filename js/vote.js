4764dd4a8276678a311b0804c1726fb559def62e
Andrew Johnston <adjohnston99@gmail.com>
2020-04-04 16:46:58 -0700

    202004041646 voting system complete

--- js/vote.js
+++ js/vote.js
@@ -1,11 +1,11 @@
 function upvote(element, author, timeCode) {
-	$.ajax("vote.php?author=" + author + "&timeCode=" + timeCode + "&newVote=true");
+	$.ajax("vote.php?author=" + author + "&timeCode=" + timeCode + "&newVote=UP");
 	$(element).toggleClass("visited");
 	$(element).parent().find(".dv").removeClass("visited");
 }
 
 function downvote(element, author, timeCode) {
-	$.ajax("vote.php?author=" + author + "&timeCode=" + timeCode + "&newVote=false");
+	$.ajax("vote.php?author=" + author + "&timeCode=" + timeCode + "&newVote=DN");
 	$(element).toggleClass("visited");
 	$(element).parent().find(".uv").removeClass("visited");
 }
