0e437d338010daae920a3d7d718ae8df4f23a84b
Hans <hansfuhrmann870@gmail.com>
2020-04-04 17:42:01 -0700

    editProf

--- profile.php
+++ profile.php
@@ -1,30 +1,50 @@
+<!DOCTYPE html>
 <?php
 	session_start();
 ?>
-<!DOCTYPE html>
+
 <html lang = "en">
 <head>
 <title>Peruse</title>
 <link rel="stylesheet" href="css/reset.css">
 <link rel="stylesheet" href="css/index.css">
 <link rel="stylesheet" href="css/profile.css">
-<link type="text/javascript"   href="js/footerscroll.js">
+<!-- jQuery CDN -->
+<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+	<!-- jQuery local fallback -->
+	<script>window.jQuery || document.write('<script src="/js/jquery-3.4.1.min.js"><\/script>')</script>
+<script src="/js/editprofile.js"></script>
+
+
 </head>
 <body>
 	<?php
 		include "header.php";
 	?>
+	<div id="row">
 	<main>
-		<form action="http://www.randyconnolly.com/tests/process.php" method="post">
+		<form>
 			<fieldset>
-				<legend>SampleProfile69</legend>
-				<img src="images/profile.jpg" alt="Sample post image">
+				<legend>My Profile</legend>
+				<img src="images/profile.jpg" alt="Profile image">
 				<p>Username: SampleProfile69</p><br>
 				<p>Password: secret</p><br>
 				<p>Email: sampleprofile@someprovider.com</p><br>
-				<p>Email me about the cool new Peruse features: Yes</p><br>
+		
 				<p>Birthdate: April 11, 1989</p><br>
-				<input type="submit" value="EDIT" class="button" id="edit"> 
+				<button type="button" id="edit">Edit Profile - Open/Close</button>
+				
+				
+			</fieldset>
+		</form>
+
+		<form action="http://www.randyconnolly.com/tests/process.php" method="post" id="edit_info">
+			<fieldset>
+				<legend>Edit Profile</legend>
+						Change profile picture: <br><input type="file" name="profile-pic" id="userpic" accept="image/png, image/jpeg, image/gif" required><br>
+						Email: <input type="email" name="email" id="mail" required><br>
+						Birthdate: <input type="date" name="birthdate" required><br>
+						<input type="submit" value="Submit" class="button"> <input type="reset" value="Reset" class="button">
 				
 			</fieldset>
 		</form>
@@ -47,6 +67,7 @@
 			<i>Copyright &copy; Fuhrmann-Johnston Productions</i>
 		</footer>
 	</section>
+		</div>
 </body>
 </html>
 
