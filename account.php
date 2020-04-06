8bfacd0eb1bc74dfd9d3d8b3dc05f7c74a906213
Hans <hansfuhrmann870@gmail.com>
2020-04-06 13:19:33 -0700

    header_account_fixed

--- account.php
+++ account.php
@@ -1,73 +1,78 @@
 <!DOCTYPE html>
 <?php
 	session_start();
-	if (!isset($_SESSION['loggedInAs'])) {
-		$_SESSION['loggedInAs'] = null;
-	}
 ?>
+
+
 <html lang = "en">
 <head>
-	<title>Peruse</title>
-	<link rel="stylesheet" href="/css/reset.css">
-	<link rel="stylesheet" href="/css/index.css">
-	<!-- <link type="text/javascript" href="js/footerscroll.js"> -->
-	<script src="/js/footer.js"></script>
+<title>Peruse</title>
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/account.css">
 
-	<!-- jQuery CDN -->
-	<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+<!-- jQuery CDN -->
+<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 	<!-- jQuery local fallback -->
 	<script>window.jQuery || document.write('<script src="/js/jquery-3.4.1.min.js"><\/script>')</script>
+<script src="/js/editprofile.js"></script>
+
+
 </head>
 <body>
-<?php
-	include "header.php";
-?>
-<div id="row">
+	<?php
+		include "header.php";
+	?>
+	<div id="row">
 	<main>
-		<!-- feed loads here -->
-		<script src="/js/vote.js"></script>
-		<script id="feed" src="/js/feed.js" data-author="<?php echo (isset($_GET['a'])? $_GET['a']:""); ?>"></script>
-	</main>
+		
+		<form>
+			<fieldset>
+				
+				<legend>My Profile</legend>
+				<img src="images/profile.jpg" alt="Profile image">
+				<p>Username: SampleProfile69</p><br>
+				<p>Password: secret</p><br>
+				<p>Email: sampleprofile@someprovider.com</p><br>
+				
+
+				<p>Birthdate: April 11, 1989</p><br>
+			
+				<button type="button" id="edit">Edit Profile - Open/Close</button>
+
+
+			</fieldset>
+		</form>
+
+		<form action="http://www.randyconnolly.com/tests/process.php" method="post" id="edit_info">
+			<fieldset>
+				<legend>Edit Profile</legend>
+						Change profile picture: <br><input type="file" name="profile-pic" id="userpic" accept="image/png, image/jpeg, image/gif" required><br>
+						Email: <input type="email" name="email" id="mail" required><br>
+						Birthdate: <input type="date" name="birthdate" required><br>
+						<input type="submit" value="Submit" class="button"> <input type="reset" value="Reset" class="button">
+
+			</fieldset>
+		</form>
+		</main>
 	<section id="secondary">
 		<div id="recommended-block">
 			<h3 id="popular">Popular Communities</h3>
 			<hr />
 			<ul>
-				<li><a href="/c/CoolCSSTricks" class="text-link">c/CoolCSSTricks</a></li>
-				<li><a href="/c/GorillaPics" class="text-link">c/GorillaPics</a></li>
-				<li><a href="/c/GoodJokes" class="text-link">c/GoodJokes</a></li>
-				<li><a href="/c/QuestionsAnswered" class="text-link">c/QuestionsAnswered</a></li>
+				<li><a href="commmunity.html" class="text-link">Cool CSS tricks</a></li>
+				<li><a href="commmunity.html" class="text-link">Gorilla Pics</a></li>
+				<li><a href="commmunity.html" class="text-link">Good jokes</a></li>
+				<li><a href="commmunity.html" class="text-link">Questions Answered</a></li>
 			</ul>
 		</div>
-		<!-- start test blocks -->
-		<div id="recommended-block">
-			<h3 id="popular">Popular Communities</h3>
-			<hr />
-			<ul>
-				<li><a href="/c/CoolCSSTricks" class="text-link">c/CoolCSSTricks</a></li>
-				<li><a href="/c/GorillaPics" class="text-link">c/GorillaPics</a></li>
-				<li><a href="/c/GoodJokes" class="text-link">c/GoodJokes</a></li>
-				<li><a href="/c/QuestionsAnswered" class="text-link">c/QuestionsAnswered</a></li>
-			</ul>
-		</div>
-		<div id="recommended-block">
-			<h3 id="popular">Popular Communities</h3>
-			<hr />
-			<ul>
-				<li><a href="/c/CoolCSSTricks" class="text-link">c/CoolCSSTricks</a></li>
-				<li><a href="/c/GorillaPics" class="text-link">c/GorillaPics</a></li>
-				<li><a href="/c/GoodJokes" class="text-link">c/GoodJokes</a></li>
-				<li><a href="/c/QuestionsAnswered" class="text-link">c/QuestionsAnswered</a></li>
-			</ul>
-		</div>
-		<!-- end test blocks -->
+					
 		<footer>
-			<a href ="/">Home</a> | <a href ="#main">Back To Top</a>
+			<a href ="index.html">Home</a> | <a href ="#main">Back To Top</a>
 			<br />
 			<i>Copyright &copy; Fuhrmann-Johnston Productions</i>
-			<script src="/js/footer.js"></script>
 		</footer>
 	</section>
-</div>
+		</div>
 </body>
 </html>
